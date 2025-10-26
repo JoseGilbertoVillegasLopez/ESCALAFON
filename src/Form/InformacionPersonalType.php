9b31d0d70a0e48b3f1973c1d8c7d98625a2a5dac
Jose Gilberto VIllegas Lopez <josegibertov2@gmail.com>
2025-10-26 01:30:47 -0600

    diseno de login bonito y con funcionalidad de autenticacion listo, redireccion segun rol de usuario, dashboard de admin creado asta el momento con una sola ruta, tabla de index de informacion personal optimizada con la info necsaria y sin basura

--- src/Form/InformacionPersonalType.php
+++ src/Form/InformacionPersonalType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\InformacionPersonal;
 use App\Entity\InformacionLaboral;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -25,10 +26,6 @@ class InformacionPersonalType extends AbstractType
             ->add('estadoCivil')
             ->add('domicilio')
             ->add('imagen')
-        ;
-        $builder
-            ->add('numeroAfiliado')
-
         ;
     }
 
