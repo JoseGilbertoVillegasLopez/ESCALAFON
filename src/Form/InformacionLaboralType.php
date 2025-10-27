43aea45903fb9f8fecf496fd5a1ac022ff90432c
Jose Gilberto VIllegas Lopez <josegibertov2@gmail.com>
2025-10-27 00:20:42 -0600

    modificacion al archivo informacionlaboral para mostrar correctamento sus datos de forma trasparente para el usuario e implementado en el formulario de informacionpersonal. al formulario de informacionpersonal tambien se modificao para mejor lectura de datos foraneos, solo falta ver como manejar el tema de la imaguen

--- src/Form/InformacionLaboralType.php
+++ src/Form/InformacionLaboralType.php
@@ -8,6 +8,9 @@ use App\Entity\InformacionPersonal;
 use App\Entity\Puesto;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,21 +20,76 @@ class InformacionLaboralType extends AbstractType
     {
         $builder
             ->add('numeroAfiliado')
-            ->add('fechaIncorporacion')
-            ->add('tipoPlaza')
-            ->add('turnoactual')
-            ->add('jornada')
-            ->add('informacionPersonal', EntityType::class, [
-                'class' => InformacionPersonal::class,
-                'choice_label' => 'id',
+            ->add('fechaIncorporacion', DateType::class, [
+
+                'widget' => 'single_text', // esto renderiza un campo de entrada de texto simple
+                'label' => 'Fecha de Incorporación', // esto establece la etiqueta del campo
+                'format' => 'yyyy-MM-dd', //esto define el formato de la fecha
             ])
+            ->add('tipoPlaza', ChoiceType::class, [
+                'label' => 'Tipo de Plaza',
+                'required' => true,
+                'choices' => [
+                    'Base' => 'true',
+                    'Temporal' => 'false',
+                ],
+                'expanded' => false,
+                'multiple' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Seleccione el tipo de plaza',
+                    'maxlength' => 10,
+                    'nimlength' => 9,
+                    'data-toggle' => 'tooltip',
+                    'title' => 'Seleccione el tipo de plaza laboral',
+                ],
+            ])
+            ->add('turnoactual', ChoiceType::class, [
+                'label' => 'Turno Actual',
+                'required' => true,
+                'choices' => [
+                    'Matutino' => 'Matutino',
+                    'Vespertino' => 'Vespertino',
+                    'Nocturno' => 'Nocturno',
+                    'Mixto' => 'Mixto',
+                ],
+                'expanded' => false,
+                'multiple' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Seleccione el turno actual',
+                    'maxlength' => 10,
+                    'nimlength' => 9,
+                    'data-toggle' => 'tooltip',
+                    'title' => 'Seleccione el turno actual de trabajo',
+                ],
+            ])
+            ->add('jornada', ChoiceType::class, [
+                'label' => 'Jornada Laboral',
+                'required' => true,
+                'choices' => [
+                    'Completa' => 'Completa',
+                    'Parcial' => 'Parcial',
+                ],
+                'expanded' => false,
+                'multiple' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Seleccione el tipo de jornada',
+                    'maxlength' => 8,
+                    'nimlength' => 7,
+                    'data-toggle' => 'tooltip',
+                    'title' => 'Seleccione el tipo de jornada laboral',
+                ],
+            ])
+
             ->add('puesto', EntityType::class, [
                 'class' => Puesto::class,
-                'choice_label' => 'id',
+                'choice_label' => 'nombre',
             ])
             ->add('categoria', EntityType::class, [
                 'class' => Categoria::class,
-                'choice_label' => 'id',
+                'choice_label' => 'nombre',
             ])
         ;
     }
--- src/Form/InformacionPersonalType.php
+++ src/Form/InformacionPersonalType.php
@@ -6,6 +6,7 @@ use App\Entity\InformacionPersonal;
 use App\Entity\InformacionLaboral;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -23,9 +24,30 @@ class InformacionPersonalType extends AbstractType
             ->add('curp')
             ->add('rfc')
             ->add('nss')
-            ->add('estadoCivil')
+            ->add('estadoCivil', ChoiceType::class, [
+                'label' => 'Estado Civil',
+                'required' => true,
+                'choices' => [
+                    'Soltero(a)' => 'false',
+                    'Casado(a)' => 'true',
+                ],
+                'expanded' => false,
+                'multiple' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Seleccione el estado civil',
+                    'maxlength' => 15,
+                    'data-toggle' => 'tooltip',
+                    'title' => 'Seleccione el estado civil',
+                ],
+            ])
             ->add('domicilio')
             ->add('imagen')
+
+            //iformationLaboral
+            ->add('informacionLaboral', InformacionLaboralType::class,[
+                'label' => false,
+            ])
         ;
     }
 
